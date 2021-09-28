0f43af9ef2be8852a2dca3feb02256d6feee6f09
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-09-27 20:08:56 -0500

    banner

---
+++ src/commons/index.ts
@@ -0,0 +1,5 @@
+import colors from "./constants/colors"
+
+export {
+    colors
+}
---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,9 @@
+import BannerImg from "../../commons/icons/banner.png";
+
+export default function Banner() {
+  return (
+    <div>
+      <img src={BannerImg} style={{ width: "100%", height: "500px" }} />
+    </div>
+  );
+}
