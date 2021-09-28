f210f21a16e342e0fe947ae66557cbec60e7f073
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-09-27 20:22:51 -0500

    Card

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,9 +1,22 @@
 import Banner from "../../components/banner/Banner";
+import CustomCard from "../../components/card/CustomCard";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <div style={{ display: "inline" }}>
+        <CustomCard />
+      </div>
+      <div style={{ display: "inline" }}>
+        <CustomCard />
+      </div>
+      <div style={{ display: "inline" }}>
+        <CustomCard />
+      </div>
+      <div style={{ display: "inline" }}>
+        <CustomCard />
+      </div>
     </div>
   );
 }
