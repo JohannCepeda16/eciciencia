be4ea8307c2ec350224101b8368f47fdb60d0c83
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-10-07 22:44:56 -0500

    Cards implementatioon

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -4,6 +4,7 @@ export default function Banner() {
   return (
     <div>
       <img src={BannerImg} style={{ width: "100%", height: "500px" }} />
+      <hr />
     </div>
   );
 }
--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,22 +1,70 @@
+import { Grid } from "@mui/material";
 import Banner from "../../components/banner/Banner";
-import CustomCard from "../../components/card/CustomCard";
+import CustomCard, { ICustomCard } from "../../components/card/CustomCard";
 
 export default function Home() {
+  const data: ICustomCard[] = [
+    {
+      title: "Titulo de la carta #1",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices at turpis vel aliquam. In in porta justo, vitae suscipit mi. Suspendisse venenatis mi vel magna aliquam mattis sed at sem. Donec non sem eu nunc accumsan maximus. Phasellus faucibus erat faucibus arcu ullamcorper efficitur tincidunt consequat lorem. Donec volutpat.",
+      img: "",
+      date: "10 Octubre 2021",
+    },
+    {
+      title: "Titulo de la carta #2",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices at turpis vel aliquam. In in porta justo, vitae suscipit mi. Suspendisse venenatis mi vel magna aliquam mattis sed at sem. Donec non sem eu nunc accumsan maximus. Phasellus faucibus erat faucibus arcu ullamcorper efficitur tincidunt consequat lorem. Donec volutpat.",
+      img: "",
+      date: "10 Octubre 2021",
+    },
+    {
+      title: "Titulo de la carta #3",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices at turpis vel aliquam. In in porta justo, vitae suscipit mi. Suspendisse venenatis mi vel magna aliquam mattis sed at sem. Donec non sem eu nunc accumsan maximus. Phasellus faucibus erat faucibus arcu ullamcorper efficitur tincidunt consequat lorem. Donec volutpat.",
+      img: "",
+      date: "10 Octubre 2021",
+    },
+  ];
   return (
-    <div>
+    <>
       <Banner />
-      <div style={{ display: "inline" }}>
-        <CustomCard />
-      </div>
-      <div style={{ display: "inline" }}>
-        <CustomCard />
-      </div>
-      <div style={{ display: "inline" }}>
-        <CustomCard />
-      </div>
-      <div style={{ display: "inline" }}>
-        <CustomCard />
-      </div>
-    </div>
+      <h1>Categoria #1</h1>
+      <Grid
+        container
+        spacing={10}
+        style={{ justifyContent: "center", alignItems: "center" }}
+      >
+        {data.map((card: ICustomCard, index: number) => (
+          <Grid item key={index}>
+            <CustomCard card={card} xs={4} />
+          </Grid>
+        ))}
+      </Grid>
+      <h1>Categoria #2</h1>
+      <Grid
+        container
+        spacing={10}
+        style={{ justifyContent: "center", alignItems: "center" }}
+      >
+        {data.map((card: ICustomCard, index: number) => (
+          <Grid item key={index}>
+            <CustomCard card={card} xs={4} />
+          </Grid>
+        ))}
+      </Grid>
+      <h1>Categoria #3</h1>
+      <Grid
+        container
+        spacing={10}
+        style={{ justifyContent: "center", alignItems: "center" }}
+      >
+        {data.map((card: ICustomCard, index: number) => (
+          <Grid item key={index}>
+            <CustomCard card={card} xs={4} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
