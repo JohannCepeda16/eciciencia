7df3a5d10b293a2c139f7c3e36e0e77eebfa42d2
jacepeda <jacepeda@indracompany.com>
2021-10-24 17:25:59 -0500

    Navigation tabs

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,6 +1,7 @@
 import Banner from "../../components/banner/Banner";
 import CardList from "../../components/card-list/CardList";
 import { dataPosters, postGraduate } from "../../assets/data/data-posters";
+import { Link, Typography, Breadcrumbs } from "@mui/material";
 
 export default function Home() {
     const posters = dataPosters;
@@ -15,14 +16,33 @@ export default function Home() {
     return (
         <>
             <Banner />
+            <Breadcrumbs
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                aria-label="breadcrumb"
+            >
+                <Link underline="hover" color="inherit" href="#maestria">
+                    Posters de maestría
+                </Link>
+                <Link underline="hover" color="inherit" href="#semilleros">
+                    Posters de semilleros
+                </Link>
+            </Breadcrumbs>
             {/* Posgrado */}
-            {graduateKeys.map((item: any, i: number) => (
-                <CardList key={i} title={item} cards={graduateValues[i]} />
-            ))}
+            <div id="maestria">
+                {graduateKeys.map((item: any, i: number) => (
+                    <CardList key={i} title={item} cards={graduateValues[i]} />
+                ))}
+            </div>
             {/* Semilleros  */}
-            {posterKeys.map((item: any, i: number) => (
-                <CardList key={i} title={item} cards={posterValues[i]} />
-            ))}
+            <div id="semilleros">
+                {posterKeys.map((item: any, i: number) => (
+                    <CardList key={i} title={item} cards={posterValues[i]} />
+                ))}
+            </div>
         </>
     );
 }
